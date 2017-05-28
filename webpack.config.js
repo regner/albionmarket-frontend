f8b126ea1830115b51f55b3c08c5bc4e2fc20818
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-28 09:32:03 -0700

    Build for production

--- webpack.config.js
+++ webpack.config.js
@@ -46,9 +46,7 @@ if (process.env.NODE_ENV === 'development') {
       }
     })
   ])
-}
-
-if (process.env.NODE_ENV === 'production') {
+} else (process.env.NODE_ENV === 'production') {
   module.exports.devtool = '#source-map'
   // http://vue-loader.vuejs.org/en/workflow/production.html
   module.exports.plugins = (module.exports.plugins || []).concat([
