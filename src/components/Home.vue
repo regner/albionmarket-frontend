a7615b219067a4381f92bbf1b618e410a330e281
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-28 01:23:51 -0700

    Added link to client download

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <h2>Albion Market: The Albion Online Market Aggregator</h2>
-    <p>Albion Market is an aggregation website. It only works if a large number of users who are playing Albion Online download and install the market uploader client. Please consider doing so by downloading it from here (link to be added soonish).</p>
+    <p>Albion Market is an aggregation website. It only works if a large number of users who are playing Albion Online download and install the distributed client that uploads market data. Please consider doing so by downloading it from <a href="https://github.com/Regner/albionmarket-client/releases">here</a>.</p>
     <resource-stats></resource-stats>
   </div>
 </template>
