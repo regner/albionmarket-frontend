94f803d1bb1ad2415300535879ceebdc30cff758
Regner Blok-Andersen <shadowdf@gmail.com>
2017-05-28 01:29:55 -0700

    Updated about page.

--- src/components/About.vue
+++ src/components/About.vue
@@ -10,6 +10,7 @@
     </ul>
     <h3>Source Code</h3>
     <p>Along with making all of the APIs available for others to use the source code for all of components that make up Albion Market are open sourced on GitHub. Feel free to check them out, open issues, and submit PRs.</p>
+    <p>The distributed client is written with Golang and relies on gopacket's pcap support. It looks for UDP packets with a specific set of bytes that identify the market packets. It then pulls out some JSON from those packets and sends the raw JSON to an HTTP ingest API. The backend is a Python Flask project that uses Postgres to store the information and exposes an HTTP API. Along with handling the ingest the backend also offers a set of APIs for getting the data from the database. The frontend is written with Vue JS and utalizes both Vue Router and VueX for routing and state management. The backend, frontend, and supporting systems (Postgres, Prometheus, etc.) are hosted in a GKE cluster (Google's hosted Kubernetes offering).</p>
     <ul>
       <li>Backend: <a href="https://github.com/Regner/albionmarket-backend">albionmarket-backend</a></li>
       <li>Frontend: <a href="https://github.com/Regner/albionmarket-frontend">albionmarket-frontend</a></li>
